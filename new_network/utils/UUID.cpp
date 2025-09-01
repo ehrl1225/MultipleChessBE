8d0f3de66a5daf746bf3fa6547438e9082768f9e
ehrl1225 <wjdwntls1225@naver.com>
2025-09-01 22:42:40 +0900

    UUID 추가

---
+++ new_network/utils/UUID.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 정주신 on 25. 9. 1..
+//
+
+#include "UUID.h"
+#include <uuid/uuid.h>
+std::string UUID::generate() {
+    uuid_t uuid;
+    uuid_generate(uuid);
+    char uuid_str[37];
+    uuid_unparse_lower(uuid, uuid_str);
+    return std::string(uuid_str);
+}
---
+++ new_network/utils/UUID.h
@@ -0,0 +1,16 @@
+//
+// Created by 정주신 on 25. 9. 1..
+//
+
+#ifndef MULTIPLECHESS_UUID_H
+#define MULTIPLECHESS_UUID_H
+
+#include <string>
+
+class UUID {
+public:
+    static std::string generate();
+};
+
+
+#endif //MULTIPLECHESS_UUID_H
